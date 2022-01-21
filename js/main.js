6faa89e1d5c57e39408dc67a45db5c1fc6067eed
Bassel <bassel444555@gmail.com>
2022-01-21 10:32:17 +0200

    Add localStorage

--- js/main.js
+++ js/main.js
@@ -7,25 +7,19 @@ let time_s = document.getElementById('time');
 let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 let cu_m = new Date().getMonth();
 window.onload = () => {
-  clear.setAttribute('disabled', '');
   creatOpt();
-  start.addEventListener('click', function() {
+  clear.setAttribute('disabled', '');
+
+
+  // Get and render local storege
+  if (localStorage.date) {
     start.setAttribute('disabled', '');
     clear.removeAttribute('disabled');
-    let t = `${month_s.value} ${day_s.value}, ${year_s.value} ${time_s.value}:00`;
-    let date = new Date(t).getTime();
     let count_down = setInterval(() => {
+      let date = new Date(localStorage.date).getTime();
       let dateNow = new Date().getTime();
       let dateDifferent = date - dateNow;
-      let days = Math.floor(dateDifferent / (1000 * 60 * 60 * 24));
-      let hours = Math.floor(dateDifferent % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-      let minutes = Math.floor(dateDifferent % (1000 * 60 * 60) / (1000 * 60));
-      let seconds = Math.floor(dateDifferent % (1000 * 60) / 1000);
-      // console.log(`${days}:${hours}:${minutes}:${seconds}`);
-      document.querySelector('[data-name=Day]').innerHTML = days;
-      document.querySelector('[data-name=Hours]').innerHTML = hours;
-      document.querySelector('[data-name=Minutes]').innerHTML = minutes;
-      document.querySelector('[data-name=Seconds]').innerHTML = seconds;
+      get_time(dateDifferent);
       if (dateDifferent <= 1) {
         cler(count_down);
       }
@@ -34,10 +28,48 @@ window.onload = () => {
       cler(count_down);
     });
 
+  }
+
+
+
+  start.addEventListener('click', function() {
+    start.setAttribute('disabled', '');
+    clear.removeAttribute('disabled');
+    let t = `${month_s.value} ${day_s.value}, ${year_s.value} ${time_s.value}:00`;
+    let date = new Date(t).getTime();
+    //  add LS
+    window.localStorage.date = t;
+    // render Count Down
+    let count_down = setInterval(() => {
+      let dateNow = new Date().getTime();
+      let dateDifferent = date - dateNow;
+      get_time(dateDifferent);
+      if (dateDifferent <= 1) {
+        cler(count_down);
+      }
+    }, 1000);
+    // Clear btn
+    clear.addEventListener('click', () => {
+      cler(count_down);
+    });
+
+
   });
 }
 
 
+function get_time(dateDifferent) {
+  let days = Math.floor(dateDifferent / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(dateDifferent % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  let minutes = Math.floor(dateDifferent % (1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor(dateDifferent % (1000 * 60) / 1000);
+  // console.log(`${days}:${hours}:${minutes}:${seconds}`);
+  document.querySelector('[data-name=Day]').innerHTML = days;
+  document.querySelector('[data-name=Hours]').innerHTML = hours;
+  document.querySelector('[data-name=Minutes]').innerHTML = minutes;
+  document.querySelector('[data-name=Seconds]').innerHTML = seconds;
+
+}
 
 function creatOpt() {
   let month_now = new Date().getMonth();
@@ -54,7 +86,7 @@ function creatOpt() {
   for (let i = 1; i <= 31; i++) {
     let opt = document.createElement('option');
     opt.textContent = i;
-    if (i == day_now + 2) {
+    if (i == day_now + 3) {
       opt.setAttribute('selected', '')
     }
     day_s.appendChild(opt);
@@ -76,5 +108,5 @@ function cler(count) {
   document.querySelector('[data-name=Minutes]').innerHTML = '0';
   document.querySelector('[data-name=Seconds]').innerHTML = '0';
   clear.setAttribute('disabled', '');
-
+  localStorage.removeItem('date');
 }
